368abc783c4232ed1f07cbe5e42432a5e82117f1
David Arce <davidarce.dia@gmail.com>
2022-05-06 02:05:57 -0400

    made random number function to generate a random number that later indicates the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-prompt("chicken")
+  function RandomNumber (min, max) {
+  return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+minChoice = 1;
+maxChoice = 3;
+
+let computerNumChoice = RandomNumber(minChoice, maxChoice);
+
+let computerPlay;
+
+switch (computerNumChoice){
+  case 1:
+    computerPlay = "Rock";
+  break;
+  case 2:
+    computerPlay = "Paper";
+  break;
+  case 3:
+    computerPlay = "Scissors";
+  break;
+}
+
