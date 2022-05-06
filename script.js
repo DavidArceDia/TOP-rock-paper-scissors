a5a898c96e035a1fa0e5ce2605bc87099f8e83f5
David Arce <davidarce.dia@gmail.com>
2022-05-06 16:14:07 -0400

    refactored and added function comments

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+/**
+ * Returns a random number within max and min.
+ * 
+ * @param {Number} min minimum value.
+ * @param {Number} max maximum value.
+ * @returns {Number} random number within max and min.
+ */
 function randomNumber(min, max) {
   return Math.floor(Math.random()*(max-min+1))+min;
 }
 
+/**
+ * Returns a string "Rock" "Paper" or "Scissors".
+ * 
+ * @param {Number} computerNumChoice Randomly selected number.
+ * @returns {String} converted number into string "Rock" "Paper" or "Scissors".
+ */
 function computerPlay(computerNumChoice){
   switch (computerNumChoice){
     case 1:
@@ -16,17 +29,20 @@ function computerPlay(computerNumChoice){
   }
 }
 
+/**
+ * Returns the result of a single Rock-Paper-Scissors game.
+ * 
+ * @param {String} computerSelection computer's choice of Rock, Paper or Scissors.
+ * @param {String} playerSelection player's choice of Rock, Paper or Scissors.
+ * @returns Result of the match.
+ */
 function playGame(computerSelection, playerSelection){
   if(computerSelection === playerSelection){
     return "Tie game";
   }
-  else if(computerSelection === "Scissors" && playerSelection === "Paper"){
-    return "Computer wins";
-  }
-  else if(computerSelection === "Paper" && playerSelection === "Rock"){
-    return "Computer wins";
-  }
-  else if(computerSelection === "Rock" && playerSelection === "Scissors"){
+  else if((computerSelection === "Scissors" && playerSelection === "Paper")
+          || (computerSelection === "Paper" && playerSelection === "Rock")
+          || (computerSelection === "Rock" && playerSelection === "Scissors")){
     return "Computer wins";
   }
   else {
@@ -34,6 +50,14 @@ function playGame(computerSelection, playerSelection){
   }
 }
 
+/**
+ * Plays 5 rounds of playGame, each with a different computerSelection - all nessisary
+ * computerSelection functions are called inside.
+ * 
+ * @param {Number} min minimum value (for randomNumber function).
+ * @param {Number} max maximum value (for randomNumber function).
+ * @returns {String} the winner of 5 matches, ties not included.
+ */
 function game(minChoice, maxChoice) {
 
   let computerWinCount = 0;
@@ -42,7 +66,6 @@ function game(minChoice, maxChoice) {
   for (let i = 0; i < 5; i++){
     
     let computerNumChoice = randomNumber(minChoice, maxChoice);   //This Produces the random computer # choice
-
     let computerSelection = computerPlay(computerNumChoice);      //This produces the final "Rock Paper Scissor" choice
     
     let playerSelection = prompt("Rock, Paper or Scissors?");     //Prompts user for their selection
@@ -50,29 +73,24 @@ function game(minChoice, maxChoice) {
     if(playGame(computerSelection, playerSelection) === "Computer wins"){
       computerWinCount++;
       console.log("Computer Wins");
-      console.log(computerSelection, playerSelection);
     }
     else if(playGame(computerSelection, playerSelection) === "Player wins"){
       playerWinCount++;
       console.log("Player Wins");
-      console.log(computerSelection, playerSelection);
     }
     else if(playGame(computerSelection, playerSelection) === "Tie game"){
-      --i;
+      i--;
       console.log("Tie");
-      console.log(computerSelection, playerSelection);
     }
-    console.log(i);
   }
 
-  return (playerWinCount > computerWinCount) ? "Player Wins!" : "Computer Wins!"
+  return (playerWinCount > computerWinCount) ? "Player Wins!" : "Computer Wins!";
 }
 
+const MIN_CHOICE = 1;    //These two choices are the upper and 
+const MAX_CHOICE = 3;    //lower bounds for the computer's Number Choice
 
-minChoice = 1;    //These two choices are the upper and 
-maxChoice = 3;    //lower bounds for the computer's Number Choice
-
-console.log(game(minChoice, maxChoice));
+console.log(game(MIN_CHOICE, MAX_CHOICE));
 
 
 
